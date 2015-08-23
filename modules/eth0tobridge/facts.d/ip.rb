90befad10a7279735a5dfe9cb3d5ccbbf0abeb6e
NikolaMandic <mn080202@gmail.com>
2015-08-23 15:15:45 +0300

    first commit

--- modules/eth0tobridge/facts.d/ip.rb
+++
@@ -1,9 +0,0 @@
-
-class dns ($interface) {
-  $address = inline_template("<%= scope.lookupvar('::ipaddress_${interface}') -%>")
-
-  $message = "So you want to know the IP address for ${interface} huh?"
-  $message += "Well, it's ${address}"
-  notify {  "${message}": }
-}
-class { 'dns': interface => 'eth0' }
