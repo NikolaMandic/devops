2183983e40c83356d546eb73d2ab5d9c99f55224
NikolaMandic <mn080202@gmail.com>
2015-09-14 23:40:42 +0300

    first commit

---
+++ resize-volumes.sh
@@ -0,0 +1,8 @@
+cd /var/lib/cinder
+mv cinder-volumes cinder-volumes.back$2
+dd if=/dev/zero of=./cinder-volumes bs=1 count=0 seek=$1
+umount /dev/loop2
+losetup /dev/loop2 cinder-volumes
+pvcreate /dev/loop2 -ff
+vgcreate cinder-volumes /dev/loop2
+service cinder-volume restart; service cinder-api restart; service cinder-scheduler restart;
